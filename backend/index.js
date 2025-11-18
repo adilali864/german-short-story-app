6b0a8c640b3472ef92adbc4f273e82d09cd9c82f
AdilKhan <asilali864@gmail.com>
2025-11-18 22:19:39 +0530

    added frontend link for cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://german-short-story-app.vercel.app",
+    ],
   })
 );
 
